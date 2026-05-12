4b7350d14737f61ff8fe3c7edd1fc5a9fab13bfb
damaru84 <info.danielrubio@gmail.com>
2026-05-12 00:11:51 -0300

    identacion y agregado del argumento  PUT

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-console.log ("inicializando el programa")
-const url_API = "https://fakestore.api.com"
-const argumentos = process.argv.slice(2)
-const argumentos_validos = ["GET", "POST","PUT", "DELETE"]
-console.log(argumentos),
+console.log ("inicializando el programa");
+const url_API = "https://fakestore.api.com";
+const argumentos = process.argv.slice(2);
+const argumentos_validos = ["GET", "POST","PUT", "DELETE"];
+
 async function programa_principal(argumentos = []){
     if(!argumentos[0] in argumentos_validos){
-        console.log ("comandos incorrectos")
+        console.log ("comandos incorrectos");
         break;
     }
     switch(argumentos[0]){
@@ -57,29 +57,46 @@ async function programa_principal(argumentos = []){
 ///////////////////////////////////////////////////////////////////////////POST///////////////////////////////////////////////////////////////////////////
             case "POST":
                 if(argumentos.length == 5 && argumentos[1] == "productos"){
-                    const [ , , nombre, precio, categoria] = argumentos
+                    const [ , , nombre, precio, categoria] = argumentos;
+                try {
                     const response = await fetch(`${url_API}/products`,{
                         method : "POST",
                         headers:  { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(
-                        {
-                            nombre, 
-                            precio,
-                            categoria
-                        }
-                    )
-                })
-               
-                if(!response.ok){
-                    throw new Error("Error en la solicitud")
+                    body: JSON.stringify({ nombre, precio, categoria })
+                });
+                if(!response.ok){throw new Error("Error en la solicitud")};
+                const data = await response.json();
+                console.log(data);
+                } catch (error) {
+                    console.log(error);
                 }
-                const data = await response.json()
-                console.log(data)
-                break
             }else{
                 console.log("Solicitud incompleta")
-                break
+                break;
             }
+/////////////////////////////////////////////////////////////////////////// PUT ///////////////////////////////////////////////////////////////////////////
+            case "PUT":
+                 if (argumentos.length == 5 && argumentos[1].includes("/") && argumentos[1].includes("products")) {
+                     let id_sin_separar = argumentos[1].split("/");
+             try {
+                const id = parseInt(id_sin_separar[1]);
+                const [ , , nombre, precio, categoria] = argumentos;
+                const response = await fetch(`${url_API}/products/${id}`, {
+                    method: "PUT",
+                    headers: { "Content-Type": "application/json" },
+                     body: JSON.stringify({ nombre, precio, categoria })
+            });
+            if (!response.ok) throw new Error("Error en la solicitud");
+            const data = await response.json();
+            console.log("Producto actualizado:", data);
+            } catch (error) {
+            console.log(error);
+            }
+        } else {
+             console.log("Solicitud incompleta o incorrecta");
+            break;
+        }
+         
 
 ///////////////////////////////////////////////////////////////////////////DELETE///////////////////////////////////////////////////////////////////////////
         case "DELETE":
@@ -87,24 +104,19 @@ async function programa_principal(argumentos = []){
                 let id_sin_separar = argumentos[1].split("/")
                 try{
                     const id = parseInt(id_sin_separar[1])
-                    const reponse = await fetch(`${url_API}/products/${id}`,{
-                        method: "DELETE"
-                    })
-                    if (!reponse.ok){
-                        throw new Error ("Error en la solicitud")
-                        break;
+                    const reponse = await fetch(`${url_API}/products/${id}`,{method: "DELETE"})
+                    if (!reponse.ok){throw new Error ("Error en la solicitud");
                     }
                     const data = await reponse.json()
                     console.log(data)
                     break;
                 }catch(error){
                     console.log(error)
-                    break;
                 }
             }else{
                 console.log("Solicitud incorrecta")
             }
+            break;
     }
 }
-
-programa_principal(argumentos)
+programa_principal(argumentos);
